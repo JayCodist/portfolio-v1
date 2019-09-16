ca3c41c80ca93fd1c0113979748ccaea8c752468
Johnson Ubaezuonu <jaycodist@gmail.com>
2019-09-16 10:52:07 +0100

    changes

--- ludybyte/scripts/index.js
+++ ludybyte/scripts/index.js
@@ -5,6 +5,7 @@ document.getElementById("login-signup-button").addEventListener("click", e =>
 	document.getElementById("signup").style.left = "100vw";
 	document.getElementById("login").className = "left-shift";
 	document.getElementById("signup").className = "left-shift";
+	setTimeout(() => document.getElementById("signup-name").focus(), 1300);
 });
 document.getElementById("signup-login-button").addEventListener("click", e => 
 {
@@ -13,4 +14,5 @@ document.getElementById("signup-login-button").addEventListener("click", e =>
 	document.getElementById("signup").style.left = "0";
 	document.getElementById("signup").className = "right-shift";
 	document.getElementById("login").className = "right-shift";
+	setTimeout(() => document.getElementById("login-email").focus(), 1300);
 });
